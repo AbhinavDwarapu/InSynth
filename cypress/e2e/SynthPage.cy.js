c48b8798e597b189500818c98977a7ee16e10ab2
Satrumidium <32928338+Satrumidium@users.noreply.github.com>
2022-07-07 02:10:57 +0200

    Added testing for all effects.

--- cypress/e2e/SynthPage.cy.js
+++ cypress/e2e/SynthPage.cy.js
@@ -6,10 +6,78 @@ describe('Check Link', () => {
   });
 });
 
-describe('Set Controller Functions', () => {
-  it('sets channel', () => {
-    cy.visit('/');
-    cy.get('a[href*="synth"]').click();
-    cy.url().should('include', '/synth');
+describe('Use keyboard as piano', () => {
+  it('presses keys: \'asdfghjk\'', () => {
+    cy.get('body').type('asdfghjk', { delay: 300 });
+  });
+});
+
+describe('Test and Play Each Effect', () => {
+  it('Play bitcrusher effect', () => {
+    cy.get('div[id=bitcrusher]').within(() => {
+      // Enable bitcrusher
+      cy.get('input').eq(0).click({ force: true });
+      // Move slider 1 using arrow keys
+      cy.get('[role="slider"]').eq(0).click().type('{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}', { force: true });
+    });
+    cy.get('body').type('asdfghjk', { delay: 300 });
+    // Disable bitcrusher
+    cy.get('div[id=bitcrusher]').within(() => {
+      cy.get('input').eq(0).click({ force: true });
+    });
+  });
+  it('Play chorus effect', () => {
+    cy.get('div[id=chorus]').within(() => {
+      cy.get('input').eq(0).click({ force: true });
+      cy.get('[role="slider"]').eq(0).click().type('{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}', { force: true });
+      cy.get('[role="slider"]').eq(1).click().type('{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}', { force: true });
+      cy.get('[role="slider"]').eq(2).click().type('{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}', { force: true });
+      cy.get('[role="slider"]').eq(3).click().type('{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}', { force: true });
+      cy.get('[role="slider"]').eq(4).click().type('{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}', { force: true });
+    });
+    cy.get('body').eq(0).type('asdfghjk', { delay: 300 });
+
+    cy.get('div[id=chorus]').within(() => {
+      cy.get('input').eq(0).click({ force: true });
+    });
+  });
+
+  it('Play distortion effect', () => {
+    cy.get('div[id=distortion]').within(() => {
+      cy.get('input').eq(0).click({ force: true });
+      cy.get('[role="slider"]').eq(0).click().type('{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}', { force: true });
+      cy.get('[role="slider"]').eq(1).click().type('{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}', { force: true });
+    });
+    cy.get('body').eq(0).type('asdfghjk', { delay: 300 });
+
+    cy.get('div[id=distortion]').within(() => {
+      cy.get('input').eq(0).click({ force: true });
+    });
+  });
+
+  it('Play reverb effect', () => {
+    cy.get('div[id=reverb]').within(() => {
+      cy.get('input').eq(0).click({ force: true });
+      cy.get('[role="slider"]').eq(0).click().type('{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}', { force: true });
+    });
+    cy.get('body').eq(0).type('asdfghjk', { delay: 300 });
+
+    cy.get('div[id=reverb]').within(() => {
+      cy.get('input').eq(0).click({ force: true });
+    });
+  });
+
+  it('Play phaser effect', () => {
+    cy.get('div[id=phaser]').within(() => {
+      cy.get('input').eq(0).click({ force: true });
+      cy.get('[role="slider"]').eq(0).click().type('{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}', { force: true });
+      cy.get('[role="slider"]').eq(1).click().type('{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}', { force: true });
+      cy.get('[role="slider"]').eq(2).click().type('{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}', { force: true });
+    });
+    cy.get('body').eq(0).type('asdfghjk', { delay: 300 });
+
+    cy.get('div[id=phaser]').within(() => {
+      cy.get('input').eq(0).click({ force: true });
+    });
   });
 });
